b6ba87ab7e2681fff3f55ba89e6aa27896c302e1
Yampi Gomez <yampigomez05@gmail.com>
2026-03-17 15:08:49 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App(){
+return (
+  <div className="min-h-screen bg-blue-800 flex items-center justify-center">
+    <h1 className="text-white text-4xl font-bold">
+      Con Yampi es Ricoprogramar...
+    </h1>
+  </div>
+)
+}
